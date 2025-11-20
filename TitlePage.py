4adf55190fd7eb51fd59a349ca99c87b86663a92
Rubyfier <rainpilv13@gmail.com>
2025-11-20 13:50:01 +0200

    commit

--- TitlePage.py
+++ TitlePage.py
@@ -1,10 +1,12 @@
-# import pygame, sys 
-# from config import *
+# # import pygame, sys 
+# # from config import *
 
-# pygame.init()
+# # pygame.init()
 
-# screen =  pygame.display.set_mode((win_width, win_height))
-# pygame.display.set_caption("ROBOLAHING")
+# # screen =  pygame.display.set_mode((win_width, win_height))
+# # pygame.display.set_caption("ROBOLAHING")
 
-# bg = pygame.color.Color('black')
-# font = pygame.font.SysFont('Arial', 60)
+# # bg = pygame.color.Color('black')
+# # font = pygame.font.SysFont('Arial', 60)
+
+# hello girly
