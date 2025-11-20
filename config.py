13f43b1dc57b34ff0080e5601a371f1f3224faca
gerdasiitan <gerda.siitan13@gmail.com>
2025-11-20 18:40:39 +0200

    Copied from "Gerda" branch

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ win_width = 640
 win_height = 480
 
 PLAYER_LAYER = 2
+NPC_LAYER = 2   
 GROUND_LAYER = 1
 
 tile_size = 64
--- sprites.py
+++ sprites.py
@@ -17,7 +17,25 @@ class Player(pygame.sprite.Sprite):
         self.height = tile_size
         
         self.image = pygame.Surface((self.width, self.height))
-        self.image = pygame.image.load("img/capybara.png")
+        self.images = {
+            "S": pygame.transform.scale(
+                pygame.image.load("img/emperor_S.png"),
+                (self.width, self.height)
+            ),
+            "W": pygame.transform.scale(
+                pygame.image.load("img/emperor_W.png"),
+                (self.width, self.height)
+            ),
+             "A": pygame.transform.scale(
+                pygame.image.load("img/emperor_A.png"),
+                (self.width, self.height)
+            ),
+             "D": pygame.transform.scale(
+                pygame.image.load("img/emperor_D.png"),
+                (self.width, self.height)
+            )
+        }
+        self.image = self.images["S"]
 
         self.rect = self.image.get_rect()
         self.rect.x = self.x
@@ -27,12 +45,41 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rect.x -= 5
+            self.image = self.images["A"]
         if keys[pygame.K_d]:
             self.rect.x += 5
+            self.image = self.images["D"]
         if keys[pygame.K_w]:
             self.rect.y -= 5
+            self.image = self.images["W"]
         if keys[pygame.K_s]:
             self.rect.y += 5
+            self.image = self.images["S"]
     
     def update(self):
         self.movement()
+
+class ArchE(pygame.sprite.Sprite):
+    def __init__(npc, game, x, y):
+        npc.game = game
+        npc.groups = npc.game.all_sprites
+        pygame.sprite.Sprite.__init__(npc, npc.groups)
+
+        npc._layer = NPC_LAYER
+
+        npc.x = x * tile_size
+        npc.y = y * tile_size
+        npc.width = tile_size
+        npc.height =  tile_size
+
+        npc.image = pygame.transform.scale(
+            pygame.image.load("img/arche.png").convert_alpha(),
+            (npc.width, npc.height)
+        )
+
+        npc.rect = npc.image.get_rect()
+        npc.rect.x = npc.x
+        npc.rect.y = npc.y
+        
+def update(npc):
+        pass
